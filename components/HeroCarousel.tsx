4c70b5b9439a46dac46e1f45032b64c03423b753
Dev Kiddz <denngodfirst@gmail.com>
2026-06-05 21:27:19 +0100

    fixed the banner section

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,26 +1,23 @@
-'use client';
+// 'use client';
 
-import React from 'react';
-import useEmblaCarousel from 'embla-carousel-react';
-import Autoplay from 'embla-carousel-autoplay';
+// import { useRef } from 'react';
+// import { useInView } from './shared/useInView';
+import Image from 'next/image';
+import image from '@/public/assets/Image-6.jpg';
 
 export default function HeroCarousel() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  // const ref = useRef(null);
+  // const isVisible = useInView(ref);
 
   return (
-    //<section className="h-[40vh] md:h-[60vh] w-full ">
-    <div className="embla mx-auto h-84 bg-muted rounded-md overflow-hidden">
-      <div className="embla__viewpor h-full" ref={emblaRef}>
-        <div className="embla__container">
-          <div className="embla__slide flex items-center justify-center">Slide 1</div>
-          <div className="embla__slide flex items-center justify-center">Slide 2</div>
-          <div className="embla__slide flex items-center justify-center">Slide 3</div>
-        </div>
-      </div>
-
-      <button className="embla__prev">Scroll to prev</button>
-      <button className="embla__next">Scroll to next</button>
-    </div>
-    // </section>
+    <section className="relative aspect-5/2 contain">
+      <Image
+        src={image}
+        alt="Picture of the author"
+        placeholder="blur" // Optional: gives a smooth blur-up effect while loading
+        fill
+        className="w-full h-auto object-cover object-center"
+      />
+    </section>
   );
 }
