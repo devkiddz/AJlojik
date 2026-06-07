84233c00ba5cece9a22ae0fcf8c63a506dbdbbd3
Dev Kiddz <denngodfirst@gmail.com>
2026-06-07 13:11:30 +0100

    fixed category z-index

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,7 +2,7 @@
 
 export default function Categories() {
   return (
-    <div className="flex items-center px-2 md:px-4 py-2 w-full sticky top-0 bg-primary-foreground/80 z-10 backdrop-blur-lg shadow-lg overflow-hidden">
+    <div className="flex items-center px-2 md:px-4 py-2 w-full sticky top-0 bg-primary-foreground/80 backdrop-blur-lg shadow-lg overflow-hidden">
       <h1> Categories</h1>
     </div>
   );
