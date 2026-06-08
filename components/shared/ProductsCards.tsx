7256d7ce6800a375332177ddfbc00254c10a8fb2
Dev Kiddz <denngodfirst@gmail.com>
2026-06-08 20:07:23 +0100

    short amendment

--- components/shared/ProductsCards.tsx
+++ components/shared/ProductsCards.tsx
@@ -7,6 +7,7 @@ import RatingComponent from './RatingComponent';
 import { Button } from '../ui/button';
 import { ChartColumnStacked, Wine } from 'lucide-react';
 import LikedComponent from './LikedComponent';
+import Link from 'next/link';
 
 type ProductCardProps = {
   products: ProductsType;
@@ -35,34 +36,43 @@ export default function ProductCard({ products }: ProductCardProps) {
           whileHover={{ scale: 1.05, y: -4 }}
           transition={{ type: 'spring', stiffness: 300 }}
           className="rounded-xl border bg-background p-4 shadow-sm">
-          <div className="relative h-60 bg-muted rounded-lg mb-3">
-            <Image src={item.images} alt={item.name} fill className="object-cover object-bottom rounded-lg" />
-            <LikedComponent productId={item.id} liked={item.liked} />
-          </div>
-          <div>
-            <h3 className="font-semibold rounded-full inset pt-2 text-sm md:text-md">{item.name}</h3>
-            <span className="text-primary flex items-center gap-1 pb-2 text-xs">
-              <ChartColumnStacked className="w-3 h-3 text-rose-500 rounded-full" />
-              {item.category}
-            </span>
-            <p className="text-xs text-muted-foreground mt-1 line-clamp-2">{item.shortDescription}</p>
-          </div>
-          <div>
-            <RatingComponent rating={item.rating} reviews={item.reviews} />
-          </div>
-          <div className="flex items-center justify-between gap-0.5 text-sm md:text-md mt-2 font-bold text-rose-500 pb-2">
-            <div>
-              <span className="">₦</span>
-              {item.price}
+          <Link href={`/products/${item.id}`} aria-label="">
+            <div className="relative aspect-4/3 bg-muted rounded-lg mb-3 overflow-hidden">
+              <Image
+                src={item.images}
+                alt={item.name}
+                fill
+                className="object-cover object-bottom rounded-lg"
+              />
+              <LikedComponent productId={item.id} liked={item.liked} />
             </div>
+            <div>
+              <h3 className="font-semibold rounded-full inset pt-2 text-sm md:text-md line-clamp-1">
+                {item.name}
+              </h3>
+              <span className="text-primary flex items-center gap-1 pb-2 text-xs border-b border-b-muted mb-5">
+                <ChartColumnStacked className="w-3 h-3 text-rose-500 rounded-full" />
+                {item.category}
+              </span>
+              <p className="text-xs text-muted-foreground mt-1 line-clamp-2">{item.shortDescription}</p>
+            </div>
+            <div>
+              <RatingComponent rating={item.rating} reviews={item.reviews} />
+            </div>
+            <div className="flex items-center justify-between gap-0.5 text-sm md:text-md mt-2 font-bold text-rose-500 pb-2">
+              <div>
+                <span className="">₦</span>
+                {item.price}
+              </div>
 
-            <div>
-              <Button size="sm" variant="outline" className="hover:bg-background transition-all">
-                <Wine />
-                Add Item
-              </Button>
+              <div>
+                <Button size="sm" variant="outline" className="hover:bg-background transition-all">
+                  <Wine />
+                  Add Item
+                </Button>
+              </div>
             </div>
-          </div>
+          </Link>
         </motion.div>
       ))}
     </>
