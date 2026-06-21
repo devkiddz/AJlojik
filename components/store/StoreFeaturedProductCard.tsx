a4cf71bb77b2c5c286feb6f0fee460c1a6439af1
Dev Kiddz <denngodfirst@gmail.com>
2026-06-21 14:05:35 +0100

    fixed more of responsive mobile feel

--- components/store/StoreFeaturedProductCard.tsx
+++ components/store/StoreFeaturedProductCard.tsx
@@ -33,22 +33,22 @@ export default function ProductCard({ product, onSelect, onPreview, onToggleLike
       <div className="grid grid-cols-5 justify-between items-center px-2 w-full md:min-h-75 group overflow-hidden rounded-md transition-all bg-card/50 hover:bg-card">
         {/* FEATURED PRODUCT IMAGE */}
 
-        <div className="col-span-2 relative aspect-10/8 md:aspect-16/16">
+        <div className="col-span-2 relative aspect-10/10 md:aspect-16/16">
           <Image src={activeVariant.image} alt={product.name} fill className="object-cover rounded-md" />
         </div>
 
         {/* FEATURED PRODUCT CONTENTS */}
         {/* CONTENT DETAILS */}
         <div className="p-4 items-center justify-center col-span-3">
-          <h3 className="line-clamp-1 text-md md:text-2xl font-semibold pr-4">{product.name}</h3>
-          <p className="mt-2 line-clamp-2 text-xs md:text-sm text-muted-foreground">
+          <h3 className="line-clamp-1 text-xs md:text-2xl font-semibold pr-2">{product.name}</h3>
+          <p className="mt-1 md:mt-2 line-clamp-2 text-[10px] md:text-sm text-muted-foreground">
             {product.shortDescription}
           </p>
 
           {/* category + PREVIEW */}
-          <div className="mt-3 flex items-cent gap-4">
-            <span className="flex items-center gap-1 text-xs md:text-sm text-primary">
-              <ChartColumnStacked className="h-3 w-3 md:h-5 md:w-5 text-rose-500" />
+          <div className="mt-1 md:mt-3 flex items-cent gap-4">
+            <span className="flex items-center gap-1 text-[10px] md:text-sm text-primary">
+              <ChartColumnStacked className="h-2 w-2 md:h-5 md:w-5 text-rose-500" />
               {product.category}
             </span>
 
@@ -63,14 +63,14 @@ export default function ProductCard({ product, onSelect, onPreview, onToggleLike
                   onSelect();
                 }
               }}
-              className="flex items-center gap-2 rounded-full border px-3 py-1 text-xs md:text-sm transition hover:bg-muted cursor-pointer">
+              className="flex items-center gap-2 rounded-full border px-3 py-1 text-[10px] md:text-sm transition hover:bg-muted cursor-pointer">
               Preview
-              <Eye className="h-3 w-3 md:h-5 md:w-5" />
+              <Eye className="h-2 w-2 md:h-5 md:w-5" />
             </button>
           </div>
 
           {/* SIZE + PRICE */}
-          <div className="mt-4 flex items-end justify-between gap-4">
+          <div className="mt-1 md:mt-4 flex items-end justify-between gap-4">
             {/* Prevent clicks inside this interactive area from bubbling out to the card container */}
             <div className="flex flex-col items-start gap-2 w-full" onClick={e => e.stopPropagation()}>
               <div className="flex w-full justify-between items-center">
@@ -86,14 +86,16 @@ export default function ProductCard({ product, onSelect, onPreview, onToggleLike
                 </div>
               </div>
 
-              <div className="flex justify-between items-center w-full gap-4 mt-2">
+              <div className="flex justify-between items-center w-full gap-2 md:gap-4 mt-2">
                 <Select
                   value={selectedVariantId}
                   onValueChange={value => {
                     if (value) setSelectedVariantId(value);
                   }}>
                   {/* Intercept the trigger click to preserve state focus rules */}
-                  <SelectTrigger className="h-8 w-[100px]" onClick={e => e.stopPropagation()}>
+                  <SelectTrigger
+                    className="text-[10px] md:text-sm h-3 md:h-8 w-[100px]"
+                    onClick={e => e.stopPropagation()}>
                     <SelectValue placeholder="Select Size" />
                   </SelectTrigger>
 
