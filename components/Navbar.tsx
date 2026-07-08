2d4d46210eaffb14e4dc1f9a1ec77eed07ff2280
Dev Kiddz <denngodfirst@gmail.com>
2026-07-08 08:05:16 +0100

    fix rotate control

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -118,7 +118,7 @@ export default function NavbarComponent({ brandName, brandSlug }: BrandType) {
             title="Nav Trigger"
             onClick={() => setMobileToolsOpen(!mobileToolsOpen)}
             className="md:hidden flex h-8 w-8 items-center justify-center rounded-full bg-muted hover:bg-muted/80">
-            <TextSearch className={`h-4 w-4 transition-transform ${mobileToolsOpen ? 'rotate-90' : ''}`} />
+            <TextSearch className={`h-4 w-4 transition-transform ${mobileToolsOpen ? 'rotate-180' : ''}`} />
           </button>
         </div>
       </div>
